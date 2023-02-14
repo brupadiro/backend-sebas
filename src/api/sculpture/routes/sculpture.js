6a3024f441f83881da726214199a7af8371fbe1f
brupadiro <brunodiharce@gmail.com>
2023-02-14 11:59:46 -0300

    init commit

---
+++ src/api/sculpture/routes/sculpture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sculpture router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sculpture.sculpture');
---
+++ src/api/sculpture/services/sculpture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sculpture service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sculpture.sculpture');
