6eee8adfb0dbe79e222472212fbad91374973987
brupadiro <brunodiharce@gmail.com>
2023-02-21 11:15:30 -0300

    Agregando subcarpetas a todo

---
+++ src/api/publicity/controllers/publicity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * publicity controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::publicity.publicity');
---
+++ src/api/publicity/routes/publicity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * publicity router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::publicity.publicity');
