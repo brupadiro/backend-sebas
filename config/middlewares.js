ddc80b0bbd3a125953a978cd088ed609b9eadcd6
brupadiro <brunodiharce@gmail.com>
2023-02-23 15:35:54 -0300

    cors en imagenes de strapi

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,23 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+
+
+module.exports = ({ env }) => [
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          directives: {
+            'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+            'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `tatudev.nyc3.digitaloceanspaces.com`],
+          },
+        }
+      },
+    },
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::favicon',
+    'strapi::public',
 ];
